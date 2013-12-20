6fab41d41e3c926490a3faa843f406f95b80fc5e
Robo <hop2deep@gmail.com>
2013-12-20 13:59:00 +0530

    support for transform and flags

--- index.js
+++ index.js
@@ -5,28 +5,63 @@ var browserify = require('browserify');
 var path = require('path');
 
 function error(str) {
-	gutil.log('gulp-browserify: ', gutil.colors.red(err));
+	gutil.log('gulp-browserify: ', gutil.colors.red(str));
 }
 
 module.exports = function(opts) {
-	var opts = opts || {};
+    var ctrOpts = {};
 	var buffer = '';
 	var bundler;
 
     return es.map(function (file, cb) {
         try {
-            bundler = browserify(file.path, opts);
-            bundler.bundle().on('data', function(data) {
-                buffer += data;
-            }).once('end', function() {
-            	var newFile = new gutil.File({
-      				cwd: file.cwd,
-      				base: file.base,
-      				contents: new Buffer(buffer)
-    			});
+            ctrOpts.entries = path.resolve(file.path);
 
-    			cb(null, newFile)
-            });
+            if(opts.noParse) {
+                ctrOpts.noParse = opts.noParse.map(function(filepath) {
+                    return path.resolve(filepath);
+                })
+                delete opts.noParse;
+            }
+
+            bundler = browserify(ctrOpts);
+            bundler.on('error', function(err) {
+                error(err);
+            })
+
+            if(opts.transform) {
+                opts.transform.forEach(function(transform) {
+                    console.log(file.path);
+                    bundler.transform(transform);
+                })
+            }
+            
+            if(opts.preBundleCB) {
+                opts.preBundleCB(bundler);
+            }
+
+            var onBundleComplete = function(err, src) {
+                if(err) {
+                    error(err);
+                }
+
+                var newFile = new gutil.File({
+                    cwd: file.cwd,
+                    base: file.base,
+                    contents: new Buffer(src)
+                });
+
+                cb(null, newFile)
+            }
+
+            bundler.bundle(opts, function(err, src) {
+                if(opts.postBundleCB) {
+                    opts.postBundleCB(err, src, onBundleComplete)
+                }
+                else {
+                    onBundleComplete(err, src);
+                }
+            })
         } catch (err) {
             error(err.message);
         }
