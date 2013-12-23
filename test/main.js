5eb6ed2da4aac16a882dde85e0856753de92732e
deepak1556 <hop2deep@gmail.com>
2013-12-23 22:38:20 +0530

    simple tests

--- test/main.js
+++ test/main.js
@@ -1,19 +1,37 @@
 var gulp = require('gulp');
 var gulpB = require('../');
 var expect = require('chai').expect;
-var fs = require('fs');
-var path = require('path');
 var es = require('event-stream');
+var path = require('path');
 var browserify = require('browserify');
 
 describe('gulp-browserify', function() {
-	it('should bundle modulees', function(done) {
-		var testFile = path.join(__dirname, 'test.js');
+	var testFile = path.join(__dirname, './test.js');
+	var fileContents;
+
+	beforeEach(function(done) {
 		gulp.src(testFile)
 			.pipe(gulpB())
 			.pipe(es.map(function(file){
-				expect(file.contents).to.be.an.instanceof(Buffer);
+				fileContents = file.contents;
 				done();
 			}))
+	})
+
+	it('should return a buffer', function(done) {
+		expect(fileContents).to.be.an.instanceof(Buffer);
+		done();
+	})
+	
+	it('should bundle modules', function(done) {
+		var b = browserify();
+		var chunk = '';
+		b.add(testFile)
+		b.bundle().on('data', function(data) {
+			chunk += data;
+		}).on('end', function() {
+			expect(fileContents.toString()).to.equal(chunk);
+			done();
+		})
 	})
 })
