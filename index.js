d0abd167008d8e0abea312c191901c087b9f3f47
Steve Lacy <me@slacy.me>
2013-12-29 18:37:07 -0700

    Re-write for proper gulp returns.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var path = require('path');
 
 
 function error(str) {
-	gutil.log('gulp-browserify: ', gutil.colors.red(str));
+  gutil.log('gulp-browserify: ', gutil.colors.red(str));
 }
 
 module.exports = function(opts) {
@@ -22,80 +22,56 @@ module.exports = function(opts) {
     buffer.push(file);
   }
 
-  function browserifyStream() {
-    if (buffer.length === 0) return this.emit('end');
+  function browserifyStream(file, cb) {
 
-    var self = this;
 
-    buffer.map(function (file) {
-        if (gutil.isNull(file.contents)) return cb(null, file); // pass along
-        if (gutil.isStream(file.contents)) {
-          itsAStream = true;
-          ctrOpts.basedir = file.base;
-          ctrOpts.entries = file.contents;
-        }
-        if(gutil.isBuffer(file.contents)) {
+      if (gutil.isNull(file.contents)) return cb(null, file); // pass along
+      if (gutil.isStream(file.contents)) {
+        ctrOpts.basedir = file.base;
+        ctrOpts.entries = file.contents;
+      }
+      if(gutil.isBuffer(file.contents)) {
 
-          itsABuffer = true;
-          ctrOpts.basedir = file.base;
-          temp.push(file.contents);
-          ctrOpts.entries = es.readArray(temp);
-        }else {
-          ctrOpts.entries = path.resolve(file.path);
-        }
+        ctrOpts.basedir = file.base;
+        temp.push(file.contents);
+        ctrOpts.entries = es.readArray(temp);
+      }else {
+        ctrOpts.entries = path.resolve(file.path);
+      }
 
-        if(opts.noParse) {
-          ctrOpts.noParse = opts.noParse.map(function(filepath) {
-            return path.resolve(filepath);
-          });
-          delete opts.noParse;
-        }
-
-        bundler = browserify(ctrOpts);
-        bundler.on('error', function(err) {
-          error(err);
+      if(opts.noParse) {
+        ctrOpts.noParse = opts.noParse.map(function(filepath) {
+          return path.resolve(filepath);
         });
+        delete opts.noParse;
+      }
 
-        if(opts.transform) {
-          opts.transform.forEach(function(transform) {
-            console.log(file.path);
-            bundler.transform(transform);
-          });
+      bundler = browserify(ctrOpts);
+      bundler.on('error', function(err) {
+        error(err);
+      });
+
+      if(opts.transform) {
+        opts.transform.forEach(function(transform) {
+          console.log(file.path);
+          bundler.transform(transform);
+        });
+      }
+
+      
+
+      var onBundleComplete = function(err, src) {
+        if(err) {
+          error(err);
         }
+        file.contents = new Buffer(src);
+        cb(null, file);
+      };
 
-        self.emit('prebundle', bundler);
 
-        var onBundleComplete = function(self, err, src) {
-          if(err) {
-            error(err);
-          }
-
-          var newFile = new gutil.File({
-            cwd: file.cwd,
-            base: file.base,
-            contents: new Buffer(src)
-          });
-
-          self.emit('postbundle', src);
-
-          self.emit('data', newFile);
-          self.emit('end');
-        };
-
-        if(itsAStream || itsABuffer ) {
-          var readable = bundler.bundle(opts);
-          readable.on('data', function(data) {
-            chunk += data;
-          }).once('end', function(err) {
-            onBundleComplete(self, err, chunk);
-          });
-        } else {
-          bundler.bundle(opts, function(err, src) {
-            onBundleComplete(self, err, src);
-          });
-        }
-    });
-	}
-
-	return es.through(bufferContents, browserifyStream);
+      bundler.bundle(opts, function(err, src) {
+        onBundleComplete(err, src);
+      });
+    }
+  return es.map(browserifyStream);
 };
