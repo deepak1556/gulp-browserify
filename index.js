c276b58fd4092a3cadb31eed384578e0f0f62986
deepak1556 <hop2deep@gmail.com>
2013-12-31 12:13:50 +0530

    try/catch nt needed

--- index.js
+++ index.js
@@ -30,76 +30,72 @@ module.exports = function(opts) {
     	var self = this;
 
     	 buffer.map(function (file) {
-        	try {
-            	if(isStream(file.contents)) {
+            if(isStream(file.contents)) {
 
-                	itsAStream = true;
-               		ctrOpts.basedir = file.base;
-                	ctrOpts.entries = file.contents;
-            	}else if(isBuffer(file.contents)) {
+                itsAStream = true;
+               	ctrOpts.basedir = file.base;
+                ctrOpts.entries = file.contents;
+            }else if(isBuffer(file.contents)) {
 
-                	itsABuffer = true;
-                	ctrOpts.basedir = file.base;
-                	temp.push(file.contents);
-                	ctrOpts.entries = es.readArray(temp);
-            	}else {
+                itsABuffer = true;
+                ctrOpts.basedir = file.base;
+                temp.push(file.contents);
+                ctrOpts.entries = es.readArray(temp);
+            }else {
 
-                	ctrOpts.entries = path.resolve(file.path);
-            	}
+                ctrOpts.entries = path.resolve(file.path);
+            }
 
-            	if(opts.noParse) {
-                	ctrOpts.noParse = opts.noParse.map(function(filepath) {
-                    	return path.resolve(filepath);
-                	})
-                	delete opts.noParse;
-            	}
+            if(opts.noParse) {
+                ctrOpts.noParse = opts.noParse.map(function(filepath) {
+                    return path.resolve(filepath);
+                })
+                delete opts.noParse;
+            }
 
-            	bundler = browserify(ctrOpts);
-            	bundler.on('error', function(err) {
-                	error(err);
-            	})
+            bundler = browserify(ctrOpts);
+            bundler.on('error', function(err) {
+                error(err);
+            })
 
-            	if(opts.transform) {
-                	opts.transform.forEach(function(transform) {
-                    	console.log(file.path);
-                    	bundler.transform(transform);
-                	})
-            	}
+            if(opts.transform) {
+                opts.transform.forEach(function(transform) {
+                    console.log(file.path);
+                    bundler.transform(transform);
+                })
+            }
 
-            	self.emit('prebundle', bundler);
+            self.emit('prebundle', bundler);
 
-            	var onBundleComplete = function(self, err, src) {
-                	if(err) {
-                    	error(err);
-                	}
+            var onBundleComplete = function(self, err, src) {
+                if(err) {
+                    error(err);
+                }
 
-                	var newFile = new gutil.File({
-                    	cwd: file.cwd,
-                    	base: file.base,
-                    	contents: new Buffer(src)
-                	});
+                var newFile = new gutil.File({
+                    cwd: file.cwd,
+                    base: file.base,
+                    contents: new Buffer(src)
+                });
 
-                	self.emit('postbundle', src);
+                self.emit('postbundle', src);
 
-                	self.emit('data', newFile);
-                	self.emit('end');
-            	}
+                self.emit('data', newFile);
+                self.emit('end');
+            }
 
-            	if(itsAStream || itsABuffer ) {
-                	var readable = bundler.bundle(opts);
-                	readable.on('data', function(data) {
-                    	chunk += data;
-                	}).once('end', function(err) {
-                    	onBundleComplete(self, err, chunk);
-                	})
-            	} else {
-                	bundler.bundle(opts, function(err, src) {
-                    	onBundleComplete(self, err, src);
-                	})
-            	}
-        	} catch (err) {
-            	error(err.message);
-        	}
+            if(itsAStream || itsABuffer ) {
+                var readable = bundler.bundle(opts);
+                readable.on('data', function(data) {
+                    chunk += data;
+                }).once('end', function(err) {
+                    onBundleComplete(self, err, chunk);
+                })
+            } else {
+                bundler.bundle(opts, function(err, src) {
+                    onBundleComplete(self, err, src);
+                })
+            }
     	});
 	}
 
